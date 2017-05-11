e7e7b10380cd196650a7ec3a5e7e40c667db20a1
Vitali Müller <vitalimueller@gmx.de>
2017-05-11 22:11:51 +0200

    Klasse zum Laden von Karten aus einer Datei und Test dafür hinzugefügt

---
+++ Auto-Quartett_WindowsForms.Tests/DatenZugriffTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Auto_Quartett_WindowsForms.Tests
+{
+    [TestClass]
+    public class DatenZugriffTests
+    {
+        [TestMethod]
+        public void LadeKartenTest()
+        {
+            //Arrange
+            DatenZugriff datenZugriff = new DatenZugriff(@"..\..\TestDateien\TestKartenDatei.xml");
+
+            //Act
+            Autokarte[] ergebnis = datenZugriff.LadeKarten();
+
+            //Assert
+            Assert.IsNotNull(ergebnis);
+            Assert.AreEqual(2, ergebnis.Length);
+            Assert.AreEqual("Mini", ergebnis[0].modell);
+            Assert.AreEqual(3, ergebnis[0].verbrauch);
+            Assert.AreEqual(8, ergebnis[0].ladevolumen);
+            Assert.AreEqual(123, ergebnis[1].geschwindigkeit);
+        }
+    }
+}
---
+++ Auto-Quartett_WindowsForms/DatenZugriff.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Auto_Quartett_WindowsForms
+{
+    public class DatenZugriff
+    {
+        private readonly XmlSerializer xmlSerializer;
+        private readonly string dateiPfad;
+
+        public DatenZugriff(string dateiPfad)
+        {
+            this.dateiPfad = dateiPfad;
+            this.xmlSerializer = new XmlSerializer(typeof(Autokarte[]), new XmlRootAttribute("Autokarten"));
+        }
+
+        public Autokarte[] LadeKarten()
+        {
+            using (var reader = new StreamReader(this.dateiPfad))
+            {
+                return (Autokarte[]) this.xmlSerializer.Deserialize(reader);
+            }
+        }
+    }
+}
