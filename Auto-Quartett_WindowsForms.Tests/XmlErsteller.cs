0f243346eeb1720aa0922073576d14dfbd298f5c
MajaWefers <papillon87@web.de>
2017-06-12 11:38:18 +0200

    Nissan auf 250 km geändert

--- Auto-Quartett_WindowsForms.Tests/XmlErsteller.cs
+++ Auto-Quartett_WindowsForms.Tests/XmlErsteller.cs
@@ -17,7 +17,7 @@ namespace Auto_Quartett_WindowsForms.Tests
                 new Autokarte("VW", "Phaeton", 250, 309, 15.7, 12, 6, 6.7, 600, 500),
                 new Autokarte("VW", "New Beetle", 185, 85, 8.7, 4, 2, 10.9, 419, 527),
                 new Autokarte("VW", "Touareg", 225, 230, 12.2, 10, 4.9, 7.8, 600, 555),
-                new Autokarte("Nissan","GT-R", 315, 404, 11.8, 6, 3.8, 2.8, 385, 315),
+                new Autokarte("Nissan","GT-R", 250, 404, 11.8, 6, 3.8, 2.8, 385, 315),
                 new Autokarte("BMW", "X1", 205, 110, 4.5, 4,2, 9.2, 585, 505),
                 new Autokarte("Renault", "Megane", 190, 74, 5.4, 4, 1.6, 12.3, 472, 1162)
             };
